ee0794d4e9749be58980e0a889e9d3216ab79480
musanna-bs23 <hasan.almusanna@brainstation-23.com>
2026-05-23 02:35:33 +0600

    build: update admin assets for conditional logic

--- assets/admin/build/index.asset.php
+++ assets/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '5c1f28c07fb8285a3650');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '992a1db061aaadd26918');
