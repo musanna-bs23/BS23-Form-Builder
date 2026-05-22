539342490f34a801ed0c21ed8482733b08f412ae
musanna-bs23 <hasan.almusanna@brainstation-23.com>
2026-05-23 02:42:45 +0600

    build: update admin assets for advanced validation

--- assets/admin/build/index.asset.php
+++ assets/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '992a1db061aaadd26918');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '8d1edc96cf6e69dc4e93');
