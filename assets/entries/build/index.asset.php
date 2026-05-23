9839b2fbc11dee7ebcd201a4d84f7b5c995cf369
musanna-bs23 <hasan.almusanna@brainstation-23.com>
2026-05-23 07:37:42 +0600

    build: update entries assets for uploads

--- assets/entries/build/index.asset.php
+++ assets/entries/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '78057abeb98d2f6ef8fb');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '4da7889a69c15d7348bc');
